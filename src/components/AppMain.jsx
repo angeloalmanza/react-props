bf6551225fca88fb9366d2871d49e80ec280a44f
angeloalmanza <angeloalmanza26@gmail.com>
2024-12-13 16:31:33 +0100

    feat: milestone 2

--- src/components/AppMain.jsx
+++ src/components/AppMain.jsx
@@ -1,25 +1,28 @@
 import PostCard from "./PostCard";
 import PostsArray from "../data/data";
 
-const printPosts = () => {
-    return PostsArray.map((curPost) => (
+const printPosts = (posts) => {
+    return posts.map((curPost) => (
         <div className="col" key={curPost.id}>
             <PostCard
                 image={curPost.image}
                 title={curPost.title}
                 tags={curPost.tags}
                 content={curPost.content}
+                published={curPost.published}
             />
         </div>
     ))
 }
 
 const AppMain = () => {
+    const publishedPosts = PostsArray.filter(post => post.published);
+
     return(
         <main>
             <section>
                 <div className="row">
-                    {printPosts()}
+                    {publishedPosts.length > 0 ? printPosts(publishedPosts) : <p>Nessun Post disponibile</p>}
                 </div>
             </section>
         </main>
